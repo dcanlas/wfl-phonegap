1da3be78ad16191026224245297aa85d29ba9d17
Dominique Canlas <canlasdominique@gmail.com>
2016-03-01 22:25:09 -0800

    first commit

---
+++ www/js/common/app.js
@@ -0,0 +1,24 @@
+app.constant("Config", {
+  "ApiUrl": "data/feed.json",
+  "PhotoUrl": "data/photos.json",
+  "CommentUrl": "data/comments.json",
+  "FriendsUrl": "data/friends.json",
+  "MessagesUrl": "data/messages.json",
+  "MessageUrl": "data/message.json",
+  "ProductUrl": "data/products.json",
+  "WordPress": "http://example.com/",
+})
+// config contact
+app.constant("ConfigContact", {
+  "EmailId": "canlasdominique@gmail.com",
+  "ContactSubject": "Contact Us"
+})
+// config admon
+app.constant("ConfigAdmob", {
+  "interstitial": "ca-app-pub-3940256099942544/1033173712",
+  "banner": "ca-app-pub-3940256099942544/6300978111"
+})
+// push notification
+app.constant("PushNoti", {
+  "senderID": "346504465337",
+})
