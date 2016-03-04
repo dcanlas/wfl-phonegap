c41a690f71c7c9e5c8c84585ee3ff0740b3ba439
Dominique Canlas <canlasdominique@gmail.com>
2016-03-04 00:32:28 -0800

    able to do logging in.

--- www/js/common/app.js
+++ www/js/common/app.js
@@ -7,18 +7,22 @@ app.constant("Config", {
   "MessageUrl": "data/message.json",
   "ProductUrl": "data/products.json",
   "WordPress": "http://example.com/",
-})
+});
 // config contact
 app.constant("ConfigContact", {
   "EmailId": "canlasdominique@gmail.com",
   "ContactSubject": "Contact Us"
-})
+});
 // config admon
 app.constant("ConfigAdmob", {
   "interstitial": "ca-app-pub-3940256099942544/1033173712",
   "banner": "ca-app-pub-3940256099942544/6300978111"
-})
+});
 // push notification
 app.constant("PushNoti", {
   "senderID": "346504465337",
-})
+});
+//Firebase url
+app.constant("FirebaseConfig", {
+  "url": "https://dazzling-heat-6981.firebaseio.com"
+});
--- www/js/controllers/SignUpCtrl.js
+++ www/js/controllers/SignUpCtrl.js
@@ -1,7 +1,25 @@
 // Sign up page of app //
-app.controller('SignUpCtrl', ['$state','$scope', function($state, $scope) {
+app.controller('SignUpCtrl', ['$state', '$scope', 'firebaseMain', function($state, $scope, firebaseMain) {
     // sign up logic here
-    $scope.doRegister = function(){
-        $state.go('app.features');
+    $scope.credentials = {
+        name: "",
+        email: "",
+        password: ""
+    };
+
+    $scope.doRegister = function() {
+        //comment this because we don't want this anymore
+        //$state.go('app.features');
+        firebaseMain.ref.createUser({
+            email: $scope.credentials.email,
+            password: $scope.credentials.password
+        }, function (error, userData) {
+            if (error) {
+                console.log("Error creating user: ", error);
+            }
+            else {
+                console.log("Successfully created user account with uid: ", userData.uid);
+            }
+        })
     };
 }]);
