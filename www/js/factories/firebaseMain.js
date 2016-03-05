694ff0bbb657e0e52d8c6ceb65a84370ec202b66
Dominique Canlas <canlasdominique@gmail.com>
2016-03-05 14:50:10 -0800

    re-pushing

---
+++ www/js/factories/firebaseMain.js
@@ -0,0 +1,9 @@
+app.factory('firebaseMain', ['FirebaseConfig', function firebaseMain(FirebaseConfig) {
+
+    var firebaseRef = new Firebase(FirebaseConfig.url);
+
+    return {
+        'ref': firebaseRef,
+        'userRef': firebaseRef.child('users')
+    };
+}]);
