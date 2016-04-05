f6feac677740fca64c9cdd580c6b6b8121bec70d
Dominique Canlas <canlasdominique@gmail.com>
2016-04-05 00:04:44 -0700

    some more improvements, and file upload for profile pic.

--- www/js/controllers/ProfileCtrl.js
+++ www/js/controllers/ProfileCtrl.js
@@ -1,6 +1,6 @@
 /*  Profile page template */
-app.controller('ProfileCtrl', ['$scope', '$stateParams', 'camera', 'userService',
-    function profileCtrl($scope, $stateParams, camera, userService) {
+app.controller('ProfileCtrl', ['$ionicActionSheet', '$scope', '$stateParams', 'camera', 'userService',
+    function profileCtrl($ionicActionSheet, $scope, $stateParams, camera, userService) {
 
         $scope.currentUser = userService.getCurrentUser();
         console.log("currentUser: ", $scope.currentUser);
@@ -12,22 +12,50 @@ app.controller('ProfileCtrl', ['$scope', '$stateParams', 'camera', 'userService'
             });
 
         //This can only be done when you are editing your profile.
-        $scope.takePhoto = function takePhoto() {
+        $scope.uploadPhoto = function uploadPhoto() {
+            var hideSheet = $ionicActionSheet.show({
+                 buttons: [
+                   { text: '<span class="icon ion-camera"></span>Take Photo from Camera' },
+                   { text: '<span class="icon ion-images"></span>Upload Photo from Album' }
+                 ],
+                 titleText: 'Upload Profile Picture',
+                 cancelText: 'Cancel',
+                 buttonClicked: function(index) {
+                     if (index === 0) {
+                         takePhotoFromDevice('camera');
+                     }
+                     else if (index === 1) {
+                         takePhotoFromDevice('library');
+                     }
+                     return true;
+                 }
+            });
+        };
+
+        function takePhotoFromDevice(source) {
             var options = {
                 quality : 75,
                 destinationType : Camera.DestinationType.DATA_URL,
-                sourceType : Camera.PictureSourceType.CAMERA,
                 allowEdit : true,
-                encodingType: Camera.EncodingType.JPEG,
-                popoverOptions: CameraPopoverOptions,
                 targetWidth: 500,
                 targetHeight: 500,
                 saveToPhotoAlbum: false
             };
+            if (source === 'camera') {
+                options.sourceType = Camera.PictureSourceType.CAMERA;
+                options.encodingType = Camera.EncodingType.JPEG;
+            }
+            else if (source === 'library') {
+                options.sourceType = Camera.PictureSourceType.PHOTOLIBRARY;
+            }
+            else {
+                //we do nothing if there is no source.
+                return false;
+            }
             camera.getPicture(options).then(function(imageData) {
                 $scope.currentUser.picData = imageData;
-                $scope.currentUser.$save().then(function() {
-                    $scope.$apply();
+                $scope.currentUser.$save().then(function(userObj) {
+                    $scope.$digest();
                 });
             }, function err(error) {
                 console.log(error);
